585d3e0ac5b6cf5c0b8d37f5ebf839926e819f75
Albert Vallverdu <byverdu@gmail.com>
2022-09-02 15:44:04 +0100

    changed ava for jest as test framework

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -1,68 +1,61 @@
-import test from 'ava'
 import request from 'supertest'
 import { expressApp } from '../app.mjs'
 
-test.before('Server setup', t => {
-  t.context.expressApp = expressApp({ routes: [] })
-})
+describe('App', () => {
+  it('should be defined', () => {
+    expect(expressApp).toBeInstanceOf(Function)
+  })
 
-test('should be defined', t => {
-  t.is(typeof expressApp, 'function')
-})
+  it('should throw Error if no routes are specified', () => {
+    expect(() => expressApp()).toThrowError('Either routes is not defined or it is not an Array')
+  })
 
-test('should throw Error if no routes are specified', t => {
-  const error = t.throws(() => expressApp())
-  t.is(error.message, 'Either routes is not defined or it is not an Array')
-})
+  it('should throw Error if routes do not provide a valid HTTP method', () => {
+    expect(() => expressApp({ routes: [{ method: 'posted' }] })).toThrowError('posted is not a valid HTTP method \n Allowed methods are get - delete - post - put - patch')
+  })
 
-test('should throw Error if routes do not provide a valid HTTP method', t => {
-  const error = t.throws(() => expressApp({ routes: [{ method: 'posted' }] }))
-  t.is(error.message, 'posted is not a valid HTTP method \n Allowed methods are get - delete - post - put - patch')
-})
+  it('should throw Error if routes do not provide a function for the handler', () => {
+    expect(() => expressApp({ routes: [{ method: 'get', handler: true }] })).toThrowError('handler must be a function. Actual type is "boolean"')
+  })
 
-test('should throw Error if routes do not provide a function for the handler', t => {
-  const error = t.throws(() => expressApp({ routes: [{ method: 'get', handler: true }] }))
-  t.is(error.message, 'handler must be a function. Actual type is "boolean"')
-})
+  it('should throw Error if routes do not provide a string for the path', () => {
+    expect(() => expressApp({ routes: [{ method: 'get', handler: () => {}, path: true }] })).toThrowError('path must be a string. Actual type is "boolean"')
+  })
 
-test('should throw Error if routes do not provide a string for the path', t => {
-  const error = t.throws(() => expressApp({ routes: [{ method: 'get', handler: () => {}, path: true }] }))
-  t.is(error.message, 'path must be a string. Actual type is "boolean"')
-})
+  it('should throw Error if routes provide an empty string for the path', () => {
+    expect(() => expressApp({ routes: [{ method: 'get', handler: () => {}, path: '' }] })).toThrowError('path can not be an empty string')
+  })
 
-test('should throw Error if routes provide an empty string for the path', t => {
-  const error = t.throws(() => expressApp({ routes: [{ method: 'get', handler: () => {}, path: '' }] }))
-  t.is(error.message, 'path can not be an empty string')
-})
+  it('should throw Error if routes provide a string for the path that does not start with "/"', () => {
+    expect(() => expressApp({ routes: [{ method: 'get', handler: () => {}, path: 'somePath' }] })).toThrowError('path has to start with "/"')
+  })
 
-test('should throw Error if routes provide a string for the path that does not start with "/"', t => {
-  const error = t.throws(() => expressApp({ routes: [{ method: 'get', handler: () => {}, path: 'someRoute' }] }))
-  t.is(error.message, 'path has to start with "/"')
-})
+  it('should have a /health route by default', async () => {
+    const app = expressApp({ routes: [] })
+    const resp = await request(app).get('/health').send()
 
-test('should have a /health route by default', async t => {
-  const resp = await request(t.context.expressApp).get('/health').send()
+    expect(resp.ok).toEqual(true)
+    expect(resp.type).toEqual('text/html')
+    expect(resp.text).toEqual('ok')
+  })
 
-  t.is(resp.ok, true)
-  t.is(resp.type, 'text/html')
-  t.is(resp.text, 'ok')
-})
+  it('should handle 404 requests', async () => {
+    const app = expressApp({ routes: [] })
+    const resp = await request(app).get('/notFound').send()
 
-test('should handle 404 requests', async t => {
-  const resp = await request(t.context.expressApp).get('/notFound').send()
+    expect(resp.status).toEqual(404)
+    expect(resp.type).toEqual('text/html')
+    expect(resp.text).toEqual('No handler found for /notFound')
+  })
 
-  t.is(resp.type, 'text/html')
-  t.is(resp.status, 404)
-  t.is(resp.text, 'No handler found for /notFound')
-})
+  it('should register all routes passed', async () => {
+    const routes = [{ method: 'get', handler: (req, res) => { res.json({ value: 100 }) }, path: '/someRoute' }]
+    const server = expressApp({ port: 5000, routes })
 
-test('should register all routes passed', async t => {
-  const routes = [{ method: 'get', handler: (req, res) => { res.json({ value: 100 }) }, path: '/someRoute' }]
-  const server = expressApp({ port: 5000, routes })
+    const resp = await request(server).get('/someRoute').send()
 
-  const resp = await request(server).get('/someRoute').send()
-
-  t.is(resp.ok, true)
-  t.is(resp.type, 'application/json')
-  t.deepEqual(resp.body, { value: 100 })
+    expect(resp.ok).toEqual(true)
+    expect(resp.type).toEqual('application/json')
+    expect(resp.body).toEqual({ value: 100 })
+  })
 })
