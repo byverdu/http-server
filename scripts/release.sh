21034c215dcd49ca58372c3d5248ffcd32878658
Albert Vallverdu <byverdu@gmail.com>
2022-10-06 09:05:03 +0100

    wip release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-echo "Enter new Package version"
 
-read -r version
+function get_package_version {
+  npm pkg get version
+}
 
-npm version "$version"
+if [ -z "$(git status --porcelain)" ]; then 
+  echo "Package version updated to $package_version"
+else
+  echo "Package"
+fi
 
-yarn build
+
+# select version_type in major minor patch
+# do
+#   npm version "$version_type" -no-git-tag-version
+#   package_version=$(get_package_version)
+
+#   echo "Package version updated to $package_version"
+#   break
+# done
