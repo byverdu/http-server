8b6066e9379a94356671c436a878a1a4a5122a6f
Albert Vallverdu <byverdu@gmail.com>
2022-10-08 00:46:45 +0100

    updated release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -24,15 +24,17 @@ function printColors() {
 }
 
 function get_package_version {
-  npm pkg get version
+  node -p "require('./package.json').version"
 }
 
 if [ -z "$(git status --porcelain)" ]; then
   select version_type in major minor patch; do
-    npm version "$version_type" -no-git-tag-version
-    package_version=$(get_package_version)
+    npm version "$version_type"
+    package_version="v$(get_package_version)"
 
-    echo "Package version updated to $package_version"
+    git push origin "$package_version"
+
+    printColors green "Package version updated to $package_version"
     break
   done
 else
