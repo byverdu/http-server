84c12e04db84f36842ad04471b53e62c7f2421b5
Albert Vallverdu <byverdu@gmail.com>
2022-10-07 23:42:21 +0100

    wip release process

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
 function printColors() {
-	red='\033[0;31m'
-	green='\033[0;32m'
-	orange='\033[1;33m'
-	end="\033[0m"
+  red='\033[0;31m'
+  green='\033[0;32m'
+  orange='\033[1;33m'
+  end="\033[0m"
 
-	case $1 in
-	"red")
-		color=$red
-		;;
+  case $1 in
+  "red")
+    color=$red
+    ;;
 
-	"green")
-		color=$green
-		;;
+  "green")
+    color=$green
+    ;;
 
-	"orange")
-		color=$orange
-		;;
-	esac
+  "orange")
+    color=$orange
+    ;;
+  esac
 
-	echo -e "${color}$2${end}"
+  echo -e "${color}$2${end}"
 }
 
 function get_package_version {
-	npm pkg get version
+  npm pkg get version
 }
 
 if [ -z "$(git status --porcelain)" ]; then
-select version_type in major minor patch; do
-	npm version "$version_type" -no-git-tag-version
-	package_version=$(get_package_version)
+  select version_type in major minor patch; do
+    npm version "$version_type" -no-git-tag-version
+    package_version=$(get_package_version)
 
-	echo "Package version updated to $package_version"
-	break
-done
+    echo "Package version updated to $package_version"
+    break
+  done
 else
-	printColors red "You have changes pending to commit. A release needs a clean state"
+  printColors red "You have changes pending to commit. A release needs a clean state"
 
-	git status
+  git status
 fi
