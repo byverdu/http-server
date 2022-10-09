c13620ae11acbb08558e253f07de6cc43fe5883a
Albert Vallverdu <byverdu@gmail.com>
2022-10-10 00:13:12 +0100

    updated release script with previous tag

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,11 +6,12 @@ if [ -z "$(git status --porcelain)" ]; then
   select version_type in major minor patch; do
     npm version "$version_type"
     package_version="v$(get_package_version)"
+    previous_git_tag="get_previous_git_tag"
 
     echo -e "# Release notes for $package_version\n" >release.md
-    git log "$package_version"..Head --oneline >>release.md
+    git log "$previous_git_tag"..Head --oneline >>release.md
 
-    # git push origin "$package_version"
+    git push origin "$package_version"
 
     printColors green "Package version updated to $package_version"
     break
