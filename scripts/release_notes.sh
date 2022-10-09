f663eb7af109e66b5f0fd5c1e63212b19674f12d
Albert Vallverdu <byverdu@gmail.com>
2022-10-10 00:46:57 +0100

    script to create notes for github action

---
+++ scripts/release_notes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source ./scripts/utils.sh
+
+package_version="v$(get_package_version)"
+previous_git_tag="$(get_previous_git_tag)"
+
+echo -e "# Release notes for $package_version\n" >release.md
+git log "$previous_git_tag"..Head --pretty=format:'%h - %as - %s - %an' --graph >>release.md
