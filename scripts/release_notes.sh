a73966cb638662f4cdd34d51a90707c52a5a1f68
Albert Vallverdu <byverdu@gmail.com>
2022-10-10 21:06:07 +0100

    updated release notes script

--- scripts/release_notes.sh
+++ scripts/release_notes.sh
@@ -4,5 +4,7 @@ source ./scripts/utils.sh
 package_version="v$(get_package_version)"
 previous_git_tag="$(get_previous_git_tag)"
 
+printColors green "Creating release notes for $package_version"
 echo -e "# Release notes for $package_version\n" >release.md
 git log "$previous_git_tag"..Head --pretty=format:'%h - %as - %s - %an' --graph >>release.md
+printColors green "Release notes for $package_version created"
