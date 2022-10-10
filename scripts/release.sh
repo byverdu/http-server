8df8107ad0f0e2c7711bb4b1b42ccc71d67e9188
Albert Vallverdu <byverdu@gmail.com>
2022-10-10 22:17:23 +0100

    updated release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,7 @@ if [ -z "$(git status --porcelain)" ]; then
     # npm version "$version_type" -no-git-tag-version
     package_version="v$(get_package_version)"
 
-    git push --atomic origin "$package_version"
+    git push origin "$package_version"
 
     echo "Package version updated to $package_version"
     break
